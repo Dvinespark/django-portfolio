65f8546bb89cda1649098acbe8bfad8e35e21a7e
Shubhas <nimtsubash@gmail.com>
2021-08-06 09:48:18 +0545

    simple portfolio added

--- jobs/urls.py
+++ jobs/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-	path('', index, name='index')
+	path('', index, name='index'),
+	path('<int:pk>', job_detail, name='job_detail'),
 ]
--- jobs/views.py
+++ jobs/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404
 from .models import Job
 
 # Create your views here.
 def index(request):
 	jobs = Job.objects.all()
 	return render(request, 'jobs/home.html', {'jobs':jobs})
+
+def job_detail(request, pk):
+	job = get_object_or_404(Job, pk=pk)
+	return render(request, 'jobs/detail.html', {'job':job})
 	
